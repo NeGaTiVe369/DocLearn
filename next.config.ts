3fd1a13fe8d9515260fdf5386e591db6350fcf56
NeGaTiVe357 <kuzinartem74@gmail.com>
2025-07-14 19:53:43 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["doclearn.ru", "localhost:3000"],
+    },
+  },
   eslint: {
     // ❗️ production-build больше не упадёт из-за ESLint errors
     ignoreDuringBuilds: true,
