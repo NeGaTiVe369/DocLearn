6083fd45d6d64d9e8c8fe53824560c173147f50e
NeGaTiVe357 <kuzinartem74@gmail.com>
2025-07-15 18:49:27 +0300

    Create page.tsx

---
+++ src/app/Learn/page.tsx
@@ -0,0 +1,12 @@
+import { UnderDevelopment } from "@/shared/ui/UnderDevelopment/UnderDevelopment"
+
+export default function LearnPage() {
+  return (
+    <div>
+      <UnderDevelopment
+        title="Раздел 'Обучение' в разработке"
+        description="Мы готовим для вас обучающие материалы и курсы"
+      />
+    </div>
+  )
+}
