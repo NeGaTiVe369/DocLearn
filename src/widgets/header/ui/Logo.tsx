01143da38f9b66526e546f1ee314f8e6e2f585a5
NeGaTiVe357 <kuzinartem74@gmail.com>
2025-07-30 22:59:54 +0300

    Ошибка с переходом по лого

--- src/widgets/header/ui/Logo.tsx
+++ src/widgets/header/ui/Logo.tsx
@@ -1,35 +1,28 @@
-import { useRouter } from "next/navigation"
 import Image from "next/image"
 import desktopLogo from "@/../../public/logo.webp"
 import mobileLogo from "@/../../public/logoGoogle.webp"
 import styles from "./Header.module.css"
+import Link from "next/link"
 
 const Logo = () => {
-  const router = useRouter()
-
-  const handleLogoClick = () => {
-    router.push("/")
-    window.scrollTo(0, 0)
-  }
-
   return (
-    <div className={styles.logo} onClick={handleLogoClick}>
-      <picture>
-        <source srcSet={mobileLogo.src} media="(max-width: 650px)" />
-        <Image
-          src={desktopLogo}
-          alt="Logo"
-          width={150}
-          height={75}
-          priority
-          quality={100}
-          style={{
-            objectFit: "contain",
-            height: "auto",
-          }}
-        />
-      </picture>
-    </div>
+    <Link href="/" aria-label="На главную" className={styles.logo} prefetch > 
+        <picture>
+          <source srcSet={mobileLogo.src} media="(max-width: 650px)" />
+          <Image
+            src={desktopLogo}
+            alt="Logo"
+            width={150}
+            height={75}
+            priority
+            quality={100}
+            style={{
+              objectFit: "contain",
+              height: "auto",
+            }}
+          />
+        </picture>
+    </Link>
   )
 }
 
